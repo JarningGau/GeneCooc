10ceb5ffa271c14e09ad0ca8d4e12ce45f36e97e
JarningGau <jarninggau@gmail.com>
2024-02-21 13:32:04 +0800

    feat: cell markers from HCL and MCA

---
+++ R/data.R
@@ -0,0 +1,47 @@
+#' A data set containing marker genes identified in Human Cell Lanscape (HCL)
+#' in human symbols
+#'
+#' @format A data frame with 20400 rows and 2 variables:
+#' \describe{
+#'   \item{ont}{description of gene set}
+#'   \item{gene}{human gene symbols}
+#'   ...
+#' }
+#' @source \url{https://doi.org/10.1038/s41586-020-2157-4}
+"hcl_hsa"
+
+#' A data set containing marker genes identified in Human Cell Lanscape (HCL)
+#' in mouse symbols
+#'
+#' @format A data frame with 20400 rows and 2 variables:
+#' \describe{
+#'   \item{ont}{description of gene set}
+#'   \item{gene}{mouse gene symbols}
+#'   ...
+#' }
+#' @source \url{https://doi.org/10.1038/s41586-020-2157-4}
+"hcl_mmu"
+
+#' A data set containing marker genes identified in Mouse Cell Atlas (MCA)
+#' in human symbols
+#'
+#' @format A data frame with 32870 rows and 2 variables:
+#' \describe{
+#'   \item{ont}{description of gene set}
+#'   \item{gene}{human gene symbols}
+#'   ...
+#' }
+#' @source \url{https://doi.org/10.1016/j.cell.2018.02.001}
+"mca_hsa"
+
+#' A data set containing marker genes identified in Mouse Cell Atlas (MCA)
+#' in mouse symbols
+#'
+#' @format A data frame with 32870 rows and 2 variables:
+#' \describe{
+#'   \item{ont}{description of gene set}
+#'   \item{gene}{mouse gene symbols}
+#'   ...
+#' }
+#' @source \url{https://doi.org/10.1016/j.cell.2018.02.001}
+"mca_mmu"
